aa9c83d39dff61ffb87838f7fa9ad2a8c445f475
Abdallah Jaber <abdallahj708@gmail.com>
2022-12-12 11:12:25 -0600

    Project09

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,32 +1,43 @@
-/**
- *
- * FIRST, COMPLETE Shape interface
- * THEN, COMPLETE Rectangle, Square, and Circle classes
- * LASTLY, COMPLETE TestShapes class
- */
 
 //implement Shape interface and provide abstract method implementation
-public class Rectangle {
-
-    /*
-    Define instance variables of rectangle as below
-    Instance variables must be encapsulated
-    double width
-    double height
-     */
+public class Rectangle implements Shape{
 
 
 
+    private double width;
+    private double height;
 
-    /*
-    Override area and perimeter methods here
-    REMEMBER:
-    Area of a rectangle can be found as => width * height
-    Perimeter of a rectangle can be found as => 2 * (width + height)
-    */
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
 
 
 
-    //Override toString() method here
+    @Override
+    public double getArea() {
+        return height * width;
+    }
 
+    @Override
+    public double getPerimeter() {
+        return 2 * (height + width);
+    }
+
+//Override toString() method here
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
 }
